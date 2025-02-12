0dea7bdb91301573d9617982700035a80d16d30d
Palchhi8 <jainpalchhi2k6@gmail.com>
2025-02-12 11:15:43 +0530

    Milestone-15

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,SignupPage,Home,CreateProduct,MyProducts} from "./Routes";
+import {LoginPage,SignupPage,Home,CreateProduct, MyProducts, Cart} from "./Routes";
 import "./App.css";
-
 
 const App = () => {
   return (
@@ -12,8 +11,9 @@ const App = () => {
         <Route path='/login' element={<LoginPage/>}/>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/create-product" element={<CreateProduct />} />
+        <Route path="/create-product/:id" element={<CreateProduct />} />
         <Route path="/my-products" element={<MyProducts />} />
-        <Route path = "/create-product/:id" element={<CreateProduct/>}/>
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   )
